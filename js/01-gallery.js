a4d8ea48be08fabaf234c079f8221d057c085191
Valerii Pometun <vmpometun@gmail.com>
2023-02-20 23:11:17 +0200

    start task#1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+galleryRef.addEventListener("click", onImagePreviewClick);
+
+const imageMarkup = galleryItems
+  .map((item) => {
+    return `
+   <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>
+   `;
+  })
+  .join("");
+
+galleryRef.insertAdjacentHTML("beforeend", imageMarkup);
+
+function onImagePreviewClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const imageOriginal = event.target.dataset.source;
+  console.log(imageOriginal);
+
+  basicLightbox.create(imageOriginal).show();
+}
